0236df81f2efde7b77c14c67fc1198bf79a708d3
nicomontale <roxas.nm@gmail.com>
2020-07-26 11:35:40 +0200

    important color

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,24 +2,46 @@ import React, { Component } from 'react'
 import { Grid, Cell } from 'react-mdl';
 import img1 from '../components/react-me.png';
 import {Link} from 'react-router-dom';
+var nodemailer = require('nodemailer');
 
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'sriyank.siddhartha@gmail.com',
+    pass: 'xxxx'
+  }
+});
+
+var mailOptions = {
+  from: 'sriyank.siddhartha@gmail.com',
+  to: 'smartherd@gmail.com, sriyank@smartherd.com',
+  subject: 'Sending Email using Node.js',
+  text: `Hi Smartherd, thank you for your nice Node.js tutorials.
+          I will donate 50$ for this course. Please send me payment options.`
+  // html: '<h1>Hi Smartherd</h1><p>Your Messsage</p>'        
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
 export default class Contact extends Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {name: '', email:''};
     
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        
       }
     
       handleChange(event) {
-        this.setState({value: event.target.value});
+        this.setState({name: event.target.value, email: event.target.value});
       }
     
-      handleSubmit(event) {
-        alert('E\' stato inserito un nome: ' + this.state.value);
-        event.preventDefault();
-      }
+     
     render() {
         return (
             
@@ -36,16 +58,17 @@ export default class Contact extends Component {
                        
                         <div className="contact">
                             {/* Linkedin */}
-                            <form style={{borderRadius: '20px', width:'200%', height: '450%'}} onSubmit={this.handleSubmit}>
+                            <form action="nicola.montaleone95@gmail.com" method="post" encType="text/plain" style={{borderRadius: '20px', width:'200%', height: '450%'}} >
                            
         <label>
-         
-          <input placeholder="Email..." style={{marginTop:'10px'}} type="text" value={this.state.value} onChange={this.handleChange} />
+        <input placeholder="Name..." style={{marginTop:'10px'}} type="text" value={this.state.value} onChange={this.handleChange} />
+          <input placeholder="Email..." style={{marginTop:'10px'}} type="email" value={this.state.value} onChange={this.handleChange} />
           </label>
           <label>
         <textarea placeholder="Contact me.."  type="text"  style={{cursor: 'pointer'}}/>
-        <input style={{marginTop:'10px',backgroundColor: "#944e03", color: 'white'}} type="submit" value="Submit" />
         </label>
+        <input style={{marginTop:'10px',backgroundColor: "#944e03", color: 'white'}} type="submit" value="Submit" />
+        
       </form>
       
                         
